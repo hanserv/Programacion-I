fbc1c8630e81005e39863c8940120b05171a96e2
Hanser Ventura <hanserventura123@gmail.com>
2025-09-16 19:04:27 -0400

    añadiendo ejercicio 1

---
+++ Tarea-1/src/tarea/pkg1/Ejercicio1.java
@@ -0,0 +1,15 @@
+package tarea.pkg1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //1. Realizar un programa que imprima por pantalla su nombre y correo electrónico.
+        
+        //Declaracion e inicializacion de varibles.
+        String nombre = "Hanser Ventura";
+        String email = "20251027@itla.edu.do";
+        
+        //Salida por consola.
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Correo electronico: "+ email);
+    }
+}
---
+++ Tarea-1/src/tarea/pkg1/Tarea1.java
@@ -0,0 +1,9 @@
+package tarea.pkg1;
+
+public class Tarea1 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
